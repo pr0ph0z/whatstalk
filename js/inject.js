b6eec58073805e50ff5420b07a945ca2efb274b4
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-06-01 19:27:19 +0700

    Add inject.js function

--- js/inject.js
+++ js/inject.js
@@ -1,1 +1,21 @@
-//
+function loop () {
+    if (!document.getElementById("main")) {
+        setTimeout(loop, 1000);
+    } else {
+        init()
+    }
+}
+
+function init () {
+    const mutationObserver = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            console.log(mutation.target.textContent)
+        })
+    })
+    
+    mutationObserver.observe(document.querySelector('#main > header > div>div>span'), {
+        attributes: true
+    })
+}
+
+loop()
