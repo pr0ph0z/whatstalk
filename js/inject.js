e2d3eab62d7ccd9a9a46848d647cfb42ed8eb7ff
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-06-01 20:05:14 +0700

    Synchronize between observer and chrome storage (as a config)

--- js/inject.js
+++ js/inject.js
@@ -8,14 +8,31 @@ function loop () {
 
 function init () {
     const mutationObserver = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            console.log(mutation.target.textContent)
+        mutations.forEach(async function (mutation) {
+            const status = await getStatus()
+            if (status) {
+                console.log(mutation.target.textContent)
+            }
         })
     })
     
     mutationObserver.observe(document.querySelector('#main > header > div>div>span'), {
         attributes: true
     })
+}
+
+async function getStatus () {
+    const currentStatus = await (new Promise((resolve, reject) => {
+        chrome.storage.local.get('stalking', (data) => {
+            resolve(data)
+        })
+    }))
+    
+    if (Object.keys(currentStatus).length === 0) {
+        return false
+    }
+
+    return currentStatus.stalking
 }
 
 loop()
