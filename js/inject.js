06076f706ce077acc4d44bf3a06283ad4a5a3f92
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-06-27 20:55:37 +0700

    add function to insert online time to storage

--- js/inject.js
+++ js/inject.js
@@ -12,6 +12,8 @@ function init () {
             const status = await getStatus()
             if (status) {
                 if (mutation.type === 'characterData' && mutation.target.textContent === 'online') {
+                    const name = document.querySelectorAll('#main > header > div > div > div > span')[1].textContent
+                    await insertChatData({ name: name, message: mutation.target.textContent, time: new Date().toISOString()})
                     console.log(mutation.target.textContent, new Date())
                 }
             }
@@ -39,6 +41,36 @@ async function getStatus () {
     }
 
     return currentStatus.stalking
+}
+
+async function insertChatData({ name, message, time }) {
+    const chatData = await getChatData()
+    chatData.push({ id: generateGuid(), name, message, time })
+    return (new Promise((resolve, reject) => {
+        chrome.storage.local.set({ chat_data: chatData }, (data) => {
+            resolve(data)
+        })
+    }))
+}
+
+async function getChatData() {
+    const { chat_data: chatData } = await (new Promise((resolve, reject) => {
+        chrome.storage.local.get('chat_data', (data) => {
+            resolve(data)
+        })
+    }))
+
+    if (chatData === undefined) {
+        return []
+    }
+
+    return chatData
+}
+
+function generateGuid() {
+    const s4 = () => (((1+Math.random())*0x10000)|0).toString(16).substring(1)
+
+    return (s4()+s4()+"-"+s4()+"-"+s4()+"-"+s4()+"-"+s4()+s4()+s4());
 }
 
 loop()
